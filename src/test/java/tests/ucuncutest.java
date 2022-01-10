a4418bd7ce6c91f84313f5c6dbea574867f88098
abdkzl <abdkzl.nw@gmail.com>
2022-01-10 20:49:27 +0100

    tekrar

--- src/test/java/tests/ucuncutest.java
+++ src/test/java/tests/ucuncutest.java
@@ -1,10 +1,18 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ucuncutest {
     @Test(groups = "smoke")
     public  void UC() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("www.google.com");
+        Assert.assertFalse(driver.getCurrentUrl().equals("www.google.com"));
         System.out.println("ucuncu test yazildi");
     }
 }
