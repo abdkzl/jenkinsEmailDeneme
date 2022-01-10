90e44138ad90144efa7cae121adbd6706da698ba
abdkzl <abdkzl.nw@gmail.com>
2022-01-10 20:26:38 +0100

    xml files

--- src/test/java/tests/ikincitest.java
+++ src/test/java/tests/ikincitest.java
@@ -1,3 +1,5 @@
+package tests;
+
 public class ikincitest {
     public static void main(String[] args) {
         System.out.println("Ikinci test yazildi");
--- src/test/java/tests/ucuncutest.java
+++ src/test/java/tests/ucuncutest.java
@@ -1,3 +1,5 @@
+package tests;
+
 public class ucuncutest {
     public static void main(String[] args) {
         System.out.println("ucuncu test yazildi");
